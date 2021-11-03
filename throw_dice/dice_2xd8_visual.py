69b2691a64c76a75ed957f1d1e3c095f60c0758d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-11-03 17:46:56 +0100

    updadate

---
+++ throw_dice/dice_2xd8_visual.py
@@ -0,0 +1,32 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from throw_dice.dice import Dice
+
+# Utworzenie dwóch kości do gry typu D8
+dice_1 = Dice(8)
+dice_2 = Dice(8)
+
+# Wykonanie tysiąca rzutów dwoma kostkami typu D8 i  umieszczenie wyników na liście
+results = []
+for roll_num in range(1000):
+    result = dice_1.roll() + dice_2.roll()
+    results.append(result)
+
+# Analiza wyniku
+frequencies = []
+max_result = dice_1.num_sides + dice_2.num_sides
+for value in range(2, max_result + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Wizualizacja wyników
+x_values = list(range(2, max_result + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Wynik', 'dtick': 1}
+y_axis_config = {'title': 'Częstotliwość występowania wartości'}
+my_layout = Layout(title='Wynik rzucania dwoma kośćmi D8 tysiąc razy', xaxis=x_axis_config, yaxis=y_axis_config)
+
+offline.plot({'data': data, 'layout': my_layout}, filename='2xd8.html')
+
